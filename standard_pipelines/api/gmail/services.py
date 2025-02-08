9aabb66e9e618c7144ac0cb2068a3f7fc7bed5d3
Jordan Johnson <jordan@agenticbrain.com>
2025-02-08 15:51:13 -0500

    Updated gmail services code to work with new api structure

--- standard_pipelines/api/gmail/services.py
+++ standard_pipelines/api/gmail/services.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from standard_pipelines.api.services import BaseAPIManager
 from standard_pipelines.auth.models import GmailCredentials
 from sqlalchemy.exc import SQLAlchemyError
 from email.message import EmailMessage
@@ -9,30 +10,32 @@ from google.oauth2.credentials import Credentials
 import base64
 
 
-class GmailService:
-    def __init__(self):
-        self.credentials = None
-        self.client_id = None
-        self.token_uri = "https://oauth2.googleapis.com/token"
+class GmailService(BaseAPIManager):
+    def __init__(self, api_config: dict, credentials: GmailCredentials) -> None:
+        super().__init__(api_config)
+        self.user_db_credentials = credentials
+        self.google_credentials = Credentials(
+                token = self.user_db_credentials.access_token,
+                refresh_token = self.user_db_credentials.refresh_token,
+                token_uri = "https://oauth2.googleapis.com/token",
+                client_id = self.api_config['GMAIL_CLIENT_ID'],
+                client_secret = self.api_config['GMAIL_CLIENT_SECRET'],
+                scopes = self.api_config['GMAIL_SCOPES'].split()
+            )
 
-
+    @property
+    def required_config(self) -> list[str]:
+        return ['GMAIL_CLIENT_ID', 'GMAIL_CLIENT_SECRET', 'GMAIL_SCOPES']
+    
     def send_email(self, to_address, subject, body):
         try:
+            current_app.logger.info(f'Sending email to {to_address}')
             email_data = self.structure_email_data(to_address, subject, body)
             if 'error' in email_data:
                 return email_data  
 
-            google_credentials = Credentials(
-                token = self.credentials.access_token,
-                refresh_token = self.credentials.refresh_token,
-                token_uri = self.token_uri,
-                client_id = current_app.config['GMAIL_CLIENT_ID'],
-                client_secret = current_app.config['GMAIL_CLIENT_SECRET'],
-                scopes = current_app.config['GMAIL_SCOPES'].split()
-            )
-
             # Create the Gmail service object
-            service = build('gmail', 'v1', credentials=google_credentials)
+            service = build('gmail', 'v1', credentials=self.google_credentials)
             # Sends the email using the Gmail API
             message = service.users().messages().send(userId="me", body=email_data).execute()
 
@@ -43,11 +46,11 @@ class GmailService:
             status_code = e.resp.status
             error_reason = e.reason
 
-            current_app.logger.exception(f"HTTP error {status_code}: {error_reason}")
+            current_app.logger.error(f"HTTP error {status_code}: {error_reason}")
             return {'error': f'{error_reason}'}
             
         except RefreshError as e:
-            current_app.logger.exception(f'Refresh error occurred: {str(e)}')
+            current_app.logger.error(f'Refresh error occurred: {str(e)}')
             return {'error': 'Refresh error occurred'}
     
         except Exception as e:
@@ -71,22 +74,3 @@ class GmailService:
         except Exception as e:
             current_app.logger.exception(f"An unexpected error occurred while structuring email data: {e}")
             return {'error': 'An unexpected error occurred while structuring email data'}
-      
-    def set_user_credentials(self, client_id : str):
-        try:
-            credentials = GmailCredentials.query.filter_by(client_id=client_id).first()
-            if not credentials:
-                current_app.logger.exception('No credentials found for the user')
-                return {'error': 'No credentials found for the user'}
-            
-            self.credentials = credentials
-            self.client_id = client_id
-            return {'message': 'User credentials retrieved successfully'}
-        
-        except SQLAlchemyError as e:
-            current_app.logger.exception(f'Database error occurred: {e}')
-            return {'error': 'An error occurred while getting the user credentials'}
-        
-        except Exception as e:
-            current_app.logger.exception(f'An unexpected error occurred: {e}')
-            return {'error': 'An unexpected error occurred'}
