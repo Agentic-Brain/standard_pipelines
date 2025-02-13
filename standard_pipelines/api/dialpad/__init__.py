85d6b63338c2a2607e67c6570a769885ae1e1a5e
Adam <acpohl21@gmail.com>
2025-02-13 01:07:58 -0500

    removed unneeded blueprint

--- standard_pipelines/api/dialpad/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("dialpad", __name__)
-
-def init_app(app):
-    from standard_pipelines.api.dialpad import routes
-    app.register_blueprint(bp, url_prefix="/api/dialpad")
