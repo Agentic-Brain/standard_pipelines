fb1621377c2a9f083f262be153c9675da1592df8
Jordan Johnson <jordan@agenticbrain.com>
2025-02-15 23:10:40 -0500

    Improved get transcript function

--- standard_pipelines/api/dialpad/services.py
+++ standard_pipelines/api/dialpad/services.py
@@ -16,9 +16,15 @@ class DialpadAPIManager(BaseAPIManager):
     def get_transcript(self, call_id: str):
         try:
             transcript = self.dialpad_client.transcript.get(call_id=call_id)
+            lines = transcript.get("lines")
+            if not lines:
+                current_app.logger.error(f"No transcript found for call_id: {call_id}")
+                return {"error": "No transcript found"}
 
-            return {"transcript": transcript}
+            only_transcripts = [entry for entry in lines if entry['type'].lower() == 'transcript']
+            return {"transcript": only_transcripts}
 
         except Exception as e:
             current_app.logger.error(f"Error fetching transcripts: {e}")
             return {"error": str(e)}
+        
