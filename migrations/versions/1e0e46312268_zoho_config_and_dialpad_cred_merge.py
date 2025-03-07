9a609f244a13cf44a43f84cd13c706fb9ca2eef4
Adam <acpohl21@gmail.com>
2025-03-07 01:31:01 -0500

    cleaned up migrations

---
+++ migrations/versions/1e0e46312268_zoho_config_and_dialpad_cred_merge.py
@@ -0,0 +1,24 @@
+"""zoho config and dialpad cred merge
+
+Revision ID: 1e0e46312268
+Revises: 714fe394a0ab, c5c56465fe58
+Create Date: 2025-03-07 00:21:28.927545
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e0e46312268'
+down_revision = ('714fe394a0ab', 'c5c56465fe58')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
